670a7aebfd46ff2f7f8bfe0efb1c0603627aa1be
Aditya Jain <adibhansalirjn@gmail.com>
2024-09-30 15:04:51 +0530

    Multithreading concept : volatile and synchronised keywords

--- src/main/java/com/aditya/DesignPattern/Thread.java
+++ src/main/java/com/aditya/DesignPattern/Thread.java
@@ -1,41 +1,186 @@
 package com.aditya.DesignPattern;
 
+/*
+ * Approaches :
+ * 1. Making the variable as volatile
+ * 2. making the CS of thread as synchronised by block
+ * 3. making the common method as synchronised by synchronised method
+ * 4. making the part of the method as synchronised using synchronised block
+ */
 public class Thread {
+
+    // If we will not declare the turn variable as volatile
+    // Without it, threads might read a stale (cached) value, which can lead to inconsistent or incorrect behavior.
+    //variable value updates are not guaranteed to be visible across different threads.
+    // This can lead to one thread not seeing the updated value of turn, causing both threads to stop functioning properly after a certain point.
+    //private static  boolean turn = true;
+
+    /**
+     * <p>Approach 1</p>
+     */
+    //private static volatile boolean turn = true;
+
+    //    public static void main(String[] args) {
+    //
+    //        java.lang.Thread t1 = new java.lang.Thread(() -> {
+    //            while (true) {
+    //                System.out.println("1");
+    //                if (turn) {
+    //                    extracted("Hello", false);
+    //                }
+    //            }
+    //
+    //        });
+    //
+    //        java.lang.Thread t2 = new java.lang.Thread(() -> {
+    //            while (true) {
+    //                System.out.println("2");
+    //                if (!turn) {
+    //                    extracted("World", true);
+    //                }
+    //            }
+    //        });
+    //
+    //
+    //        t1.start();
+    //        t2.start();
+    //    }
+    //
+    //    private static void extracted(String Hello, boolean a) {
+    //        print(Hello);
+    //        turn = a;
+    //        try {
+    //            java.lang.Thread.sleep(100);
+    //        } catch (InterruptedException e) {
+    //            throw new RuntimeException(e);
+    //        }
+    //    }
+    //
+    //    private static void print(String name) {
+    //        System.out.println(name);
+    //    }
+
+
+    /**
+     * <p>Approach 2</p>
+     */
+    //        private static boolean turn = true;
+    //
+    //        public static void main(String[] args) {
+    //
+    //            java.lang.Thread t1 = new java.lang.Thread(() -> {
+    //                while (true) {
+    //                    System.out.println("1");
+    //                    synchronized (Thread.class) {
+    //                        if (turn) {
+    //                            extracted("Hello", false);
+    //                        }
+    //                    }
+    //                }
+    //
+    //            });
+    //
+    //            java.lang.Thread t2 = new java.lang.Thread(() -> {
+    //                while (true) {
+    //                    System.out.println("2");
+    //                    synchronized (Thread.class) {
+    //                        if (!turn) {
+    //                            extracted("World", true);
+    //                        }
+    //                    }
+    //                }
+    //            });
+    //
+    //
+    //            t1.start();
+    //            t2.start();
+    //        }
+    //
+    //        private static void extracted(String Hello, boolean a) {
+    //            print(Hello);
+    //            turn = a;
+    //            try {
+    //                java.lang.Thread.sleep(100);
+    //            } catch (InterruptedException e) {
+    //                throw new RuntimeException(e);
+    //            }
+    //        }
+    //
+    //        private static void print(String name) {
+    //            System.out.println(name);
+    //        }
+
+    /**
+     * <p>Approach 3</p>
+     */
+    //    private static boolean turn = true;
+    //
+    //    public static void main(String[] args) {
+    //
+    //        java.lang.Thread t1 = new java.lang.Thread(() -> {
+    //            while (true) {
+    //                System.out.println("1");
+    //                if (turn) {
+    //                    extracted("Hello", false);
+    //                }
+    //
+    //            }
+    //
+    //        });
+    //
+    //        java.lang.Thread t2 = new java.lang.Thread(() -> {
+    //            while (true) {
+    //                System.out.println("2");
+    //
+    //                if (!turn) {
+    //                    extracted("World", true);
+    //                }
+    //
+    //            }
+    //        });
+    //
+    //
+    //        t1.start();
+    //        t2.start();
+    //    }
+    //
+    //    private static synchronized void extracted(String Hello, boolean a) {
+    //        print(Hello);
+    //        turn = a;
+    //        try {
+    //            java.lang.Thread.sleep(100);
+    //        } catch (InterruptedException e) {
+    //            throw new RuntimeException(e);
+    //        }
+    //    }
+    //
+    //    private static void print(String name) {
+    //        System.out.println(name);
+    //    }
 
     private static boolean turn = true;
 
     public static void main(String[] args) {
 
         java.lang.Thread t1 = new java.lang.Thread(() -> {
-
             while (true) {
-                //System.out.println("1");
+                System.out.println("1");
                 if (turn) {
-                    print("Hello");
-                    turn = false;
-                    try {
-                        java.lang.Thread.sleep(100);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
+                    extracted("Hello", false);
                 }
+
             }
 
         });
 
         java.lang.Thread t2 = new java.lang.Thread(() -> {
-
             while (true) {
                 System.out.println("2");
+
                 if (!turn) {
-                    print("World");
-                    turn = true;
-                    try {
-                        java.lang.Thread.sleep(100);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
+                    extracted("World", true);
                 }
+
             }
         });
 
@@ -44,8 +189,20 @@ public class Thread {
         t2.start();
     }
 
+    private static void extracted(String Hello, boolean a) {
+        synchronized (Thread.class) {
+            print(Hello);
+            turn = a;
+        }
+
+        try {
+            java.lang.Thread.sleep(100);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     private static void print(String name) {
         System.out.println(name);
     }
-
 }
